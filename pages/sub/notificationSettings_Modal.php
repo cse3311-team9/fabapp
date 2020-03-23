2cbcd94ed68c66e5eab54a633d09075a345f5bde
Michael Teixeira <michael.teixei@gmail.com>
2020-03-23 08:49:23 -0500

    #28 add functionallity to buttons save and drop. Still trying to get the queries to work.

--- pages/sub/notificationSettings_Modal.php
+++ pages/sub/notificationSettings_Modal.php
@@ -7,10 +7,13 @@
 *	 settings for the currently logged in user.
 *
 **************************************************/
+
+include_once ($_SERVER['DOCUMENT_ROOT'].'/connections/db_connect8.php');
+include_once ($_SERVER['DOCUMENT_ROOT'].'/connections/ldap.php');
+include_once ($_SERVER['DOCUMENT_ROOT'].'/class/all_classes.php');
+
+$staff = unserialize($_SESSION['staff']);
 ?>
-<script>
-    
-</script>
 
 <div id="settingsModal" class="modal fade" role="dialog">
     <div class="modal-dialog">
@@ -19,21 +22,36 @@
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Notification Settings</h4>
             </div>
-            <div id="settingsBody" class="modal-body">
-                <div>
-                    <label>Email: </label>
-                    <input type="text" name="email" id="email" class="form-control">
+            <form name="form" action="" method="post">
+                <div id="settingsBody" class="modal-body">
+                    <div>
+                        <label>Email: </label>
+                        <input type="text" name="email" id="email" class="form-control">
+                    </div>
+                    <div>
+                        <label>Phone: </label>
+                        <input type="text" name="phone" id="phone" class="form-control">
+                    </div>
                 </div>
-                <div>
-                    <label>Phone: </label>
-                    <input type="text" name="phone" id="phone" class="form-control">
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal" style="float: right;">Cancel</button>
+                    <?php
+                        if(array_key_exists('btnSave', $_POST)) { 
+                            $query = 'UPDATE contact_info
+                                      SET email = ' . $_POST['email'] . ', phone = ' . $_POST['phone'] .
+                                     'WHERE userId = ' . $staff->operator;
+
+                            $mysqli->query($query);
+                        }
+
+                        if(array_key_exists('btnDrop', $_POST)) { 
+                            echo "Drop Clicked!!";
+                        }
+                    ?>
+                    <button type="submit" name="btnSave" class="btn btn-default" style="float: right; margin-right: 10px;">Save</button>
+                    <button type="submit" name="btnDrop" class="btn btn-default" style="float: left; background-color: red;" title="Erase contact information from system">Drop</button>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal" style="float: right;">Cancel</button>
-                <button type="button" class="btn btn-default" style="float: right; margin-right: 10px;">Save</button>
-                <button type="button" class="btn btn-default" style="float: left; background-color: red;" title="Erase contact information from system">Drop</button>
-            </div>
+            </form>
         </div>
     </div>
 </div>
