679af19d8b8941ca090025ec5fe6e63c3844b37b
Bishal Paudel <bishal.paudel11111@gmail.com>
2020-04-28 22:43:05 -0500

    Update db_connect8.php

--- connections/db_connect8.php
+++ connections/db_connect8.php
@@ -9,3 +9,7 @@ $mysqli = new mysqli($dbhost, $dbuser, $dbpass) or die(mysql_error());
 // Selecting database 
 $mysqli->select_db($dbdatabase) or die(mysql_error());
 ?>
+<?php
+    // Connecting to mysql database with Role 8
+    $mysqli = new mysqli('localhost', 'Fablabian', 'sbVaBEd3eW9dxmdb', 'fabapp') or die(mysql_error());
+?>
